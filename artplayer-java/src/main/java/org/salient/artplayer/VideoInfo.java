616cbe5b9b257163e73fa0c66e6c3b4e3b4a003b
麦文昌 <cv.stronger@gmail.com>
2019-12-16 14:03:29 +0800

    通过封装的Bean来管理视频状态

---
+++ artplayer-java/src/main/java/org/salient/artplayer/VideoInfo.java
@@ -0,0 +1,39 @@
+package org.salient.artplayer;
+
+/**
+ * 视频信息封装
+ */
+public class VideoInfo {
+
+    /**
+     * 播放地址
+     */
+    String url;
+
+    /**
+     * 标题
+     */
+    String title;
+
+    /**
+     * 封面
+     */
+    String frontCover;
+
+    /**
+     * 进度
+     */
+    int progress;
+
+    /**
+     * 亮度
+     */
+    int light;
+
+    /**
+     * 音量
+     */
+    int volume;
+
+
+}
